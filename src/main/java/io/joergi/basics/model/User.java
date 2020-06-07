66416d9ad8ff8776619b7692da24dedff5051ffd
joergi <1439809+joergi@users.noreply.github.com>
2020-06-07 15:45:48 +0200

    first commit

---
+++ src/main/java/io/joergi/basics/model/User.java
@@ -0,0 +1,15 @@
+package io.joergi.basics.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class User {
+
+    private String username;
+
+    private String email;
+
+    private String password;
+}
