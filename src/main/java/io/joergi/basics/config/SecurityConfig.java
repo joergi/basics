26d85731b4a6e9c273f2c1d0f234e63b71af41bc
joergi <1439809+joergi@users.noreply.github.com>
2020-06-12 09:50:48 +0200

    allow everything in the SecurityConfig

--- src/main/java/io/joergi/basics/config/SecurityConfig.java
+++ src/main/java/io/joergi/basics/config/SecurityConfig.java
@@ -1,7 +1,6 @@
 package io.joergi.basics.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -12,6 +11,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().anyRequest().authenticated();
+        http.httpBasic().and().authorizeRequests()
+        .anyRequest().permitAll().and().csrf().disable();
+
+              //  .antMatchers("/users/register").permitAll()
+             //   .antMatchers("/users/**").permitAll();
+
     }
 }
